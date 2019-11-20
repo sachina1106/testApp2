8d13881d0d12dc33346dbf590d4c46c360ef2865
sachina1106 <sachina.maki@gmail.com>
2019-11-20 22:30:11 +0900

    問題1修正

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.app')
 @section ('content')
 
-<h1 class="page-header">{{ Auth::user()->name }} のToDo一覧</h1>
+<h1 class="page-header">のToDo一覧</h1>
 <p class="text-right">
   <a class="btn btn-success" href="/todo/create">ToDoを追加</a>
 </p>
